0069369f52a4cf19316fa6d12f9c70e2fa348e56
ProgGit12 <f23r12@yandex.ru>
2023-02-17 05:26:19 +0300

    Сделал еще парсер, но для бокса спортсменов

---
+++ Sites_pars/Rusboxing/Sportsman(box).py
@@ -0,0 +1,92 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.chrome.options import Options
+import time
+import re
+import pandas as pd
+import numpy as np
+
+# Здесь создаю браузер
+driver = webdriver.Chrome('/Users/macbookpro/Desktop/Project/Parser/Programm/chromdriver/chromedriver')
+
+link_mass = []
+name_mass = []
+citizenship_mass = []
+birthday_mass = []
+place_of_birth_mass = []
+ccilca_sportsman_mass = []
+buttons_mass = []
+
+
+
+driver.get(f"https://rusboxing.ru/professional-boxing/boxers")
+time.sleep(4)
+
+navigation = driver.find_element(by=By.XPATH, value='/html/body/div/div[2]/div/div[2]/div[2]/div/section[2]/div/div[2]/div[2]/div').find_elements(by=By.TAG_NAME, value='div')
+
+for buttons in navigation:
+    buttons_mass.append(buttons)
+
+for click_navigations in buttons_mass:
+    click_navigations.click()
+    time.sleep(1)
+    tags_a = driver.find_element(by=By.CLASS_NAME, value='boxers__content').find_elements(by=By.TAG_NAME, value='a')
+
+    for s in tags_a:
+        link_mass.append(s.get_attribute('href'))
+
+    for link in link_mass:
+        driver.get(link)
+        time.sleep(2)
+        try:
+            li = driver.find_element(by=By.CLASS_NAME, value='boxer-card__info').find_elements(by=By.TAG_NAME, value='li')
+
+            name = 0
+            birthday = 0
+            place_of_birth = 0
+            citizenship = 0
+
+            for glav in li:
+                if re.sub(r":\n.{1,200}", "", glav.text, 1) == "Имя":
+                    name = re.sub(r".{1,200}:\W", "", glav.text, 1)
+                elif re.sub(r":\n.{1,200}", "", glav.text, 1) == "Отчество":
+                    surname = re.sub(r".{1,200}:\W", "", glav.text, 1)
+                    name = f"{name} {surname}"
+                elif re.sub(r":\n.{1,200}", "", glav.text, 1) == "День рождения":
+                    birthday = re.sub(r".{1,200}:\W", "", glav.text, 1)
+                elif re.sub(r":\n.{1,200}", "", glav.text, 1) == "Место рождения":
+                    place_of_birth = re.sub(r".{1,200}:\W", "", glav.text, 1)
+                elif re.sub(r":\n.{1,200}", "", glav.text, 1) == "Гражданство":
+                    citizenship = re.sub(r".{1,200}:\W", "", glav.text, 1)
+
+
+            name_mass.append(name)
+            birthday_mass.append(birthday)
+            place_of_birth_mass.append(place_of_birth)
+            citizenship_mass.append(citizenship)
+
+            ccilca_sportsman_mass.append(link)
+
+            time.sleep(1)
+        except:
+            pass
+        print()
+
+
+
+dfPlayer_Inf = pd.DataFrame({
+        'Name': pd.Series(name_mass, dtype='object'),
+        'Birthday': pd.Series(birthday_mass),
+        'Place_of_Birth': pd.Series(place_of_birth_mass),
+        'citizenship': pd.Series(citizenship_mass),
+
+        'Link': pd.Series(ccilca_sportsman_mass),
+    })
+
+dfPlayer_Inf.to_csv(r'/Users/macbookpro/Desktop/Project/Parser_data/Table_excel/Rusboxing(box)/Sportsman(box).csv', index=False, sep=';', encoding='utf-8-sig')
+
+driver.close()
+driver.quit()
--- Sites_pars/rttf(b_tennis)/Play.py
+++ Sites_pars/rttf(b_tennis)/Play.py
@@ -36,72 +36,64 @@ ccilca_sportsman_mass = []
 
 
 
-# for i in range(1,301):
-# driver.get(f"https://rttf.ru/players")
-driver.get(f"https://rttf.ru/players/20512")
-time.sleep(2)
-
-tr = driver.find_elements(by=By.TAG_NAME, value='tr')
+for i in range(1,301):
+    driver.get(f"https://rttf.ru/players")
+    driver.get(f"https://rttf.ru/players/20512")
+    time.sleep(2)
 
 
-Block_results_matches = driver.find_element(by=By.CLASS_NAME, value='player-results')
+    tr = driver.find_elements(by=By.TAG_NAME, value='tr')
+    Block_results_matches = driver.find_element(by=By.CLASS_NAME, value='player-results')
+    info_of_match = Block_results_matches.find_elements(by=By.TAG_NAME, value='a')
 
-info_of_match = Block_results_matches.find_elements(by=By.TAG_NAME, value='a')
+    for s in info_of_match:
+        link_mass.append(s.get_attribute('href'))
 
-for s in info_of_match:
-    print(s.get_attribute('href'))
+    for link in link_mass:
+        print(link)
 
-print()
+    section = driver.find_element(by=By.TAG_NAME, value='section')
+    span = section.find_elements(by=By.TAG_NAME, value='p')
 
-# for link in link_mass:
-#     # print(link)
-#     driver.get(link)
-#
-#     section = driver.find_element(by=By.TAG_NAME, value='section')
-#     span = section.find_elements(by=By.TAG_NAME, value='p')
-#
-#
-#     birthday = 0
-#     citizenship = 0
-#     hand = 0
-#     discharge = 0
-#     name = section.find_element(by=By.TAG_NAME, value='h1').text
-#     for glav in span:
-#         if re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "город" or re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Страна":
-#             citizenship = re.sub(r".{1,50}[:]\s", "", glav.text, 1)
-#         elif re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Дата рождения":
-#             birthday = re.sub(r".{1,50}[:]\s", "", glav.text, 1)
-#             birthday = re.sub(r"[(].{1,10}[)]", "", birthday, 1)
-#         elif re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Игровая рука":
-#             hand = re.sub(r".{1,50}[:]\s", "", glav.text, 1)
-#         elif re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Разряд":
-#             discharge = re.sub(r".{1,50}[:]\s", "", glav.text, 1)
-#
-#     name_mass.append(name)
-#     birthday_mass.append(birthday)
-#
-#     citizenship_mass.append(citizenship)
-#     hand_mass.append(hand)
-#     discharge_mass.append(discharge)
-#
-#     ccilca_sportsman_mass.append(link)
-#
-#     time.sleep(1)
-#
-#
-#
-#
-# dfPlayer_Inf = pd.DataFrame({
-#         'Name': pd.Series(name_mass, dtype='object'),
-#         'Birthday': pd.Series(birthday_mass),
-#
-#         'Citizenship': pd.Series(citizenship_mass),
-#         'Hand': pd.Series(hand_mass),
-#         'discharge': pd.Series(discharge_mass),
-#
-#         'Link': pd.Series(ccilca_sportsman_mass),
-#     })
-#
+
+    birthday = 0
+    citizenship = 0
+    hand = 0
+    discharge = 0
+    name = section.find_element(by=By.TAG_NAME, value='h1').text
+    for glav in span:
+        if re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "город" or re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Страна":
+            citizenship = re.sub(r".{1,50}[:]\s", "", glav.text, 1)
+        elif re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Дата рождения":
+            birthday = re.sub(r".{1,50}[:]\s", "", glav.text, 1)
+            birthday = re.sub(r"[(].{1,10}[)]", "", birthday, 1)
+        elif re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Игровая рука":
+            hand = re.sub(r".{1,50}[:]\s", "", glav.text, 1)
+        elif re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Разряд":
+            discharge = re.sub(r".{1,50}[:]\s", "", glav.text, 1)
+
+    name_mass.append(name)
+    birthday_mass.append(birthday)
+
+    citizenship_mass.append(citizenship)
+    hand_mass.append(hand)
+    discharge_mass.append(discharge)
+
+    ccilca_sportsman_mass.append("https://rttf.ru/players/20512")
+    time.sleep(1)
+
+
+dfPlayer_Inf = pd.DataFrame({
+        'Name': pd.Series(name_mass, dtype='object'),
+        'Birthday': pd.Series(birthday_mass, dtype='object'),
+
+        'Citizenship': pd.Series(citizenship_mass, dtype='object'),
+        'Hand': pd.Series(hand_mass, dtype='object'),
+        'discharge': pd.Series(discharge_mass, dtype='object'),
+
+        'Link': pd.Series(ccilca_sportsman_mass, dtype='object'),
+    })
+
 dfPlayer_Inf.to_csv(r'/Users/macbookpro/Desktop/Project/Parser_data/Table_excel/rttf(tennis)/Play(table-tennis).csv', index=False, sep=';', encoding='utf-8-sig')
 
 driver.close()
