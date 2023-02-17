d9a061331011d562c0be454ee49847ff6153bb11
ProgGit12 <f23r12@yandex.ru>
2023-02-17 15:18:00 +0300

    Добавил в скрипт полную инфу

--- Sites_pars/rttf(b_tennis)/Sportsman.py
+++ Sites_pars/rttf(b_tennis)/Sportsman.py
@@ -22,16 +22,22 @@ options = Options()
 
 driver = webdriver.Chrome('/Users/macbookpro/Desktop/Project/Parser/Programm/chromdriver/chromedriver', options=options)
 # driver.get("https://www.sports.ru/boxing/sportsman/")
+name_mass = []
+rating_mass = []
+birthday_mass = []
+citizenship_mass = []
+hand_mass = []
+discharge_mass = []
+base_mass = []
+trim_right_mass = []
+trim_left_mass = []
+visiting_mass = []
 
 link_mass = []
-name_mass = []
-citizenship_mass = []
-birthday_mass = []
+
 weight_mass = []
 height_mass = []
-discharge_mass = []
 place_of_birth_mass = []
-hand_mass = []
 sex_mass = []
 ccilca_sportsman_mass = []
 
@@ -39,7 +45,7 @@ ccilca_sportsman_mass = []
 
 # for i in range(1,301):
 driver.get(f"https://rttf.ru/players/")
-time.sleep(30)
+time.sleep(2)
 
 tr = driver.find_elements(by=By.TAG_NAME, value='tr')
 
@@ -54,16 +60,23 @@ for a_link in tr:
 for link in link_mass:
     # print(link)
     driver.get(link)
+    time.sleep(1)
 
-    section = driver.find_element(by=By.CLASS_NAME, value='player-results')
+    section = driver.find_element(by=By.CLASS_NAME, value='player-info')
     span = section.find_elements(by=By.TAG_NAME, value='p')
 
+    name = section.find_element(by=By.TAG_NAME, value='h1').text
+    rating = section.find_element(by=By.TAG_NAME, value='dfn').text
 
     birthday = 0
     citizenship = 0
     hand = 0
     discharge = 0
-    name = section.find_element(by=By.TAG_NAME, value='h1').text
+    base = 0
+    trim_right = 0
+    trim_left = 0
+    visiting = 0
+
     for glav in span:
         if re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "город" or re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Страна":
             citizenship = re.sub(r".{1,50}[:]\s", "", glav.text, 1)
@@ -74,16 +87,28 @@ for link in link_mass:
             hand = re.sub(r".{1,50}[:]\s", "", glav.text, 1)
         elif re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Разряд":
             discharge = re.sub(r".{1,50}[:]\s", "", glav.text, 1)
+        elif re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Основание":
+            base = re.sub(r".{1,200}:\W.\s", "", glav.text, 1)
+        elif re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Накладка справа":
+            trim_right = re.sub(r".{1,200}:\W.\s", "", glav.text, 1)
+        elif re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "Накладка слева":
+            trim_left = re.sub(r".{1,200}:\W.\s", "", glav.text, 1)
+        elif re.sub(r"[:]\s.{1,50}", "", glav.text, 1) == "На сайте с":
+            visiting = re.sub(r".{1,200}:\W.\s", "", glav.text, 1)
 
     name_mass.append(name)
+    rating_mass.append(rating)
     birthday_mass.append(birthday)
-
     citizenship_mass.append(citizenship)
     hand_mass.append(hand)
     discharge_mass.append(discharge)
+    base_mass.append(base)
+    trim_right_mass.append(trim_right)
+    trim_left_mass.append(trim_left)
+    visiting_mass.append(visiting)
 
     ccilca_sportsman_mass.append(link)
-
+    print()
     time.sleep(1)
 
 
@@ -91,13 +116,17 @@ for link in link_mass:
 
 dfPlayer_Inf = pd.DataFrame({
         'Name': pd.Series(name_mass, dtype='object'),
-        'Birthday': pd.Series(birthday_mass),
+        'Rating': pd.Series(rating_mass, dtype='object'),
+        'Birthday': pd.Series(birthday_mass, dtype='object'),
+        'Citizenship': pd.Series(citizenship_mass, dtype='object'),
+        'Hand': pd.Series(hand_mass, dtype='object'),
+        'discharge': pd.Series(discharge_mass, dtype='object'),
+        'Base': pd.Series(base_mass, dtype='object'),
+        'Trim right': pd.Series(trim_right_mass, dtype='object'),
+        'Trim left': pd.Series(trim_left_mass, dtype='object'),
+        'Visiting': pd.Series(visiting_mass, dtype='object'),
 
-        'Citizenship': pd.Series(citizenship_mass),
-        'Hand': pd.Series(hand_mass),
-        'discharge': pd.Series(discharge_mass),
-
-        'Link': pd.Series(ccilca_sportsman_mass),
+        'Link': pd.Series(ccilca_sportsman_mass, dtype='object'),
     })
 
 dfPlayer_Inf.to_csv(r'/Users/macbookpro/Desktop/Project/Parser_data/Table_excel/rttf(tennis)/Sportsman(table-tennis).csv', index=False, sep=';', encoding='utf-8-sig')
