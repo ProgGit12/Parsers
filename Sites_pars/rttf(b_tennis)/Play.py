60e4384f5f326529a3b9ab01cb7bd1f6d771d90b
ProgGit12 <f23r12@yandex.ru>
2023-02-25 16:08:10 +0300

    Исправил концептуальные баги

--- Sites_pars/rttf(b_tennis)/Play.py
+++ Sites_pars/rttf(b_tennis)/Play.py
@@ -44,13 +44,29 @@ ccilca_sportsman_mass = []
 
 link_mass = []
 
-# for i in range(1,301):
+dfPlayer_Inf = pd.DataFrame({
+        'Name player': pd.Series(name_player_mass, dtype='object'),
+        'Rating player': pd.Series(rating_player_mass, dtype='object'),
+        'Data': pd.Series(date_mass, dtype='object'),
+        'Tournament': pd.Series(tournament_mass, dtype='object'),
+        'Time match': pd.Series(time_match_mass, dtype='object'),
+        'Status': pd.Series(status_mass, dtype='object'),
+        'Col man': pd.Series(col_man_mass, dtype='object'),
+        'Origin': pd.Series(origin_mass, dtype='object'),
+        'Enemy': pd.Series(enemy_mass, dtype='object'),
+        'Rating enemy': pd.Series(rating_enemy_mass, dtype='object'),
+        'Result match': pd.Series(result_match_mass, dtype='object'),
+        'Time1': pd.Series(time1_mass, dtype='object'),
+        'Time2': pd.Series(time2_mass, dtype='object'),
+
+        'Link': pd.Series(ccilca_sportsman_mass, dtype='object'),
+})
+
+
 driver.get(f"https://rttf.ru/players/")
-time.sleep(25)
-# driver.execute_script("window.scrollTo(0, 100000)")
+time.sleep(6)
 
 tr = driver.find_elements(by=By.TAG_NAME, value='tr')
-
 
 for a_link in tr:
     try:
@@ -60,11 +76,10 @@ for a_link in tr:
         pass
 
 for link in link_mass:
-    # print(link)
     driver.get(link)
     time.sleep(1)
-    driver.execute_script("window.scrollTo(0, 7000)")
-    time.sleep(2)
+    driver.execute_script("window.scrollTo(0, 10000)")
+    time.sleep(5)
 
 
     page = requests.get(link)
@@ -114,52 +129,10 @@ for link in link_mass:
                 time2 = td[6].text
 
                 tr = tr.next_sibling
-
-
-                name_player_mass.append(name_player)
-                rating_player_mass.append(rating_player)
-                date_mass.append(date)
-                tournament_mass.append(tournament)
-                time_match_mass.append(time_match)
-                status_mass.append(status)
-                col_man_mass.append(col_man)
-                origin_mass.append(origin)
-                stage_mass.append(stage)
-                enemy_mass.append(enemy)
-                rating_enemy_mass.append(rating_enemy)
-                result_match_mass.append(result_match)
-                time1_mass.append(time1)
-                time2_mass.append(time2)
-
-                ccilca_sportsman_mass.append(link)
-
+                dfPlayer_Inf.loc[len(dfPlayer_Inf.index)] = [name_player, rating_player, date, tournament, time_match, status, col_man, origin, enemy, rating_enemy, result_match, time1, time2, link]
     except AttributeError:
         pass
-    # except:
-    #     pass
-    # print()
-    # time.sleep(1)
 
-
-
-
-dfPlayer_Inf = pd.DataFrame({
-        'Name player': pd.Series(name_player_mass, dtype='object'),
-        'Rating player': pd.Series(rating_player_mass, dtype='object'),
-        'Data': pd.Series(date_mass, dtype='object'),
-        'Tournament': pd.Series(tournament_mass, dtype='object'),
-        'Time match': pd.Series(time_match_mass, dtype='object'),
-        'Status': pd.Series(status_mass, dtype='object'),
-        'Col man': pd.Series(col_man_mass, dtype='object'),
-        'Origin': pd.Series(origin_mass, dtype='object'),
-        'Enemy': pd.Series(enemy_mass, dtype='object'),
-        'Rating enemy': pd.Series(rating_enemy_mass, dtype='object'),
-        'Result match': pd.Series(result_match_mass, dtype='object'),
-        'Time1': pd.Series(time1_mass, dtype='object'),
-        'Time2': pd.Series(time2_mass, dtype='object'),
-
-        'Link': pd.Series(ccilca_sportsman_mass, dtype='object'),
-    })
 
 dfPlayer_Inf.to_csv(r'/Users/macbookpro/Desktop/Project/Parser_data/Table_excel/rttf(tennis)/Play(table-tennis).csv', index=False, sep=';', encoding='utf-8-sig')
 
