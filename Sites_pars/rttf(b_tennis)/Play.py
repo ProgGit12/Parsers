adc137a8e7c4b23f55611e1abec8dea1113a9207
ProgGit12 <f23r12@yandex.ru>
2023-02-28 14:59:34 +0300

    change time_match, but problems witch links

--- Sites_pars/rttf(b_tennis)/Play.py
+++ Sites_pars/rttf(b_tennis)/Play.py
@@ -11,6 +11,7 @@ import numpy as np
 from bs4 import BeautifulSoup
 import requests
 import lxml
+import sys
 
 # Здесь создаю браузер и перехожу на сайт
 options = Options()
@@ -64,7 +65,8 @@ dfPlayer_Inf = pd.DataFrame({
 
 
 driver.get(f"https://rttf.ru/players/")
-time.sleep(6)
+time.sleep(10)
+
 
 tr = driver.find_elements(by=By.TAG_NAME, value='tr')
 
@@ -75,11 +77,23 @@ for a_link in tr:
     except IndexError:
         pass
 
+print(len(link_mass))
+print(link_mass[-1])
+print()
 for link in link_mass:
     driver.get(link)
     time.sleep(1)
-    driver.execute_script("window.scrollTo(0, 10000)")
-    time.sleep(5)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+
 
 
     page = requests.get(link)
@@ -107,11 +121,13 @@ for link in link_mass:
 
         for i in a:
             name_player = driver.find_element(by=By.TAG_NAME, value='h1').text
-            rating_player = driver.find_element(by=By.CLASS_NAME, value='player-info').find_element(by=By.TAG_NAME, value='h3').text
+            rating_player = driver.find_element(by=By.CLASS_NAME, value='player-info').find_element(by=By.TAG_NAME, value='h3').find_element(by=By.TAG_NAME, value='dfn').text
             date = re.search(r"\d{2}.\d{2}.\d{4}", i.text)[0]
             tournament = re.sub(r"\d{2}.\d{2}.\d{4}\s\d{2}:\d{2}\s|\d{2}.\d{2}.\d{4}\s", "", i.text, 1)
             if re.search(r"\d{2}:\d{2}", i.text) != None:
                 time_match = re.search(r"\d{2}:\d{2}", i.text)[0]
+            else:
+                time_match = 0
             status = re.sub(r"\d{2}.\d{2}.\d{4}\s\d{2}:\d{2}\s|\d{2}.\d{2}.\d{4}\s", "", i.text, 1)
 
             table = player_results.find('a', attrs={"href": f'{i.get("href")}'}).find_next_sibling("table")
@@ -130,7 +146,10 @@ for link in link_mass:
 
                 tr = tr.next_sibling
                 dfPlayer_Inf.loc[len(dfPlayer_Inf.index)] = [name_player, rating_player, date, tournament, time_match, status, col_man, origin, enemy, rating_enemy, result_match, time1, time2, link]
-    except AttributeError:
+    except:
+        print(link)
+        e = sys.exc_info()[1]
+        print(e.args[0])
         pass
 
 
